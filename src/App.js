afd1c0368a54907220ea38b27f41a4bf93db1ded
jimohsegun1 <jimohsegunj@gmail.com>
2023-07-26 20:00:09 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [input, setInput] = useState('');
+
+  const handleButtonClick = (value) => {
+    setInput((prevInput) => prevInput + value);
+  };
+
+  const handleClear = () => {
+    setInput('');
+  };
+
+  const handleCalculate = () => {
+    try {
+      const result = eval(input);
+      setInput(result.toString());
+    } catch (error) {
+      setInput('Error');
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">{input}</div>
+      <div className="keypad">
+        <button onClick={handleClear} className="btn-clear">C</button>
+        <button onClick={() => handleButtonClick('/')}>&divide;</button>
+        <button onClick={() => handleButtonClick('*')}>&times;</button>
+        <button onClick={() => handleButtonClick('7')}>7</button>
+        <button onClick={() => handleButtonClick('8')}>8</button>
+        <button onClick={() => handleButtonClick('9')}>9</button>
+        <button onClick={() => handleButtonClick('-')}>-</button>
+        <button onClick={() => handleButtonClick('4')}>4</button>
+        <button onClick={() => handleButtonClick('5')}>5</button>
+        <button onClick={() => handleButtonClick('6')}>6</button>
+        <button onClick={() => handleButtonClick('+')}>+</button>
+        <button onClick={() => handleButtonClick('1')}>1</button>
+        <button onClick={() => handleButtonClick('2')}>2</button>
+        <button onClick={() => handleButtonClick('3')}>3</button>
+        <button onClick={handleCalculate} className="btn-equal">=</button>
+        <button onClick={() => handleButtonClick('0')}>0</button>
+        <button onClick={() => handleButtonClick('.')}>.</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
